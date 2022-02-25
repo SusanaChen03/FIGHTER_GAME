bdaeec9acc27ff75823615ae6c27cb8a014d11b8
SusanaChen <grupochen@hotmail.com>
2022-02-26 00:32:38 +0100

    inicio de funciones click en la pagina

--- JS/choose.js
+++ JS/choose.js
@@ -1,18 +1,30 @@
-let botonseleccionadoj1;
+let botonseleccionadoj1 = ''
 function dimegamer (miobj){
-    debugger;
+    
     let button = miobj;
 
-    if(botonseleccionadoj1 !=undefined){
+    if(botonseleccionadoj1 !=''){
 
         let ultimoboton = document.getElementById(botonseleccionadoj1);
         ultimoboton.classList.remove("selected");
     }
 
+    botonseleccionadoj1=button.id;
+    button.classList.add("selected");
+}
+
+
+let botonseleccionadoj1 = ''
+function dimegamer2 (miobj){
+    
+    let button = miobj;
+
+    if(botonseleccionadoj1 !=''){
+
+        let ultimoboton = document.getElementById(botonseleccionadoj1);
+        ultimoboton.classList.remove("selected");
+    }
 
     botonseleccionadoj1=button.id;
     button.classList.add("selected");
-         
-    
-
 }
