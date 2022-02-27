ac3ca4c4b18c7029862b64bdcd4f275b3ac687be
SusanaChen <grupochen@hotmail.com>
2022-02-27 20:54:19 +0100

    ataque 1 realizado

--- JS/playscreen.js
+++ JS/playscreen.js
@@ -1,21 +1,44 @@
 let tipojugador1="ryu";
 let tipojugador2="honda";//ryu,honda,chunli,ken
 
-
+let lifeplayer1 = 100;
+let lifeplayer2 = 100;
 
 function punetazo(){
 
     document.getElementById("player1").src="../Images/ryu/ryu.gif";
-
-    const volver =()=> document.getElementById("player1").src="../Images/ryu/R en guardia.png"
     
-    setTimeout (volver,2000)
+    let valor = 60;
+    restavidaoponente("player1", valor);
+
+    setTimeout (volver,2000);
+}
+   
+
+function volver (){ 
+        document.getElementById("player1").src="../Images/ryu/R en guardia.png";
+
+        if ( lifeplayer2 <= 0){
+            alert("jugador 1 has ganado");
+        }
+        
 }
 
+function restavidaoponente(jugador, valor){
+    
+    if (jugador == "player1"){
+        lifeplayer2 = lifeplayer2 - valor;
+    
+        document.getElementById("bv2").style="width: " + lifeplayer2 +"px;";
+        
+        
+    }
 
-
-
-
+    if (jugador == "player2"){
+        document.getElementById("bv1").style="width: " + valor +"px;";
+        lifeplayer1 = lifeplayer1 - valor;
+    }
+    }
 
 
 
