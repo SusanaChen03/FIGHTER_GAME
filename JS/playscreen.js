467183d21728e5fd62281b3bcb47c2ad504d3322
SusanaChen <grupochen@hotmail.com>
2022-02-27 15:52:57 +0100

    añadir funcion de ataque jugador 1

--- JS/playscreen.js
+++ JS/playscreen.js
@@ -1,7 +1,29 @@
+let tipojugador1="ryu";
+let tipojugador2="honda";//ryu,honda,chunli,ken
 
-function ataque1 (id){
 
-    document.getElementById('player1').src = 'display:flex';
+
+function ataquepatada (id){
+
+    document.getElementById(id).src = imagenpatada();
+    
+}
+    
+
+
+
+function imagenpatadajugador1 (){
+    
+    let v = "../Images/ryu/R patada.png";
+
+    return v;
+} 
+
+
+
+function blood1 (id,vsize){
+
+    document.getElementById(id).style="width:" + vsize +"px;";
 
 }
-
+ 
